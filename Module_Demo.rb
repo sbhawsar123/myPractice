663912ed1d578ec2f0dde4bec89af1090fb23011
Shubahm Bhawsar <sbhawsar@grepruby.io>
2021-03-05 12:23:13 +0530

    Syntax changed

--- Module_Demo.rb
+++ Module_Demo.rb
@@ -1,5 +1,6 @@
-module Calc
- def Calc.result(quant, price, rate1, rate2)
+module Calculate
+
+ def self.result(quant, price, rate1, rate2)
     return quant * price * (100 + rate1 ) * ( 100 + rate2 ) / 10000
  end
 end
